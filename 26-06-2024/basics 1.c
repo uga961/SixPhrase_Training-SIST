617fc0408e26628c7434a2d497f1c7c3ecd8c3b4
VADDI UGANDHAR <137391299+uga961@users.noreply.github.com>
2025-06-23 07:29:55 +0530

    Add files via upload

---
+++ 26-06-2024/basics 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 25;
+    printf("%d", a++);
+    printf("%d", ++a);
+    printf("%d", --a);
+    printf("%d", a--);
+    printf("%d", a);
+    return 0;
+}
